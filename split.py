62547969e194a4dbacac160669e6cebc2cb9be66
Tahoor Ahmad <mtahoorali@gmail.com>
2024-04-05 03:13:37 -0700

    code

---
+++ split.py
@@ -0,0 +1,3 @@
+import splitfolders
+dr = 'Image'
+splitfolders.ratio(dr,"splitdataset48x48" ,ratio=(0.8,0.2))
---
+++ tarin.py
@@ -0,0 +1,80 @@
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import TensorBoard
+import os
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+)
+
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+batch_size = 32
+
+train_generator = train_datagen.flow_from_directory(
+    'splitdataset48x48/train',
+    target_size=(350, 350),
+    batch_size=batch_size,
+    class_mode='categorical',
+    color_mode='grayscale'
+)
+
+validation_generator = val_datagen.flow_from_directory(
+    'splitdataset48x48/val',
+    target_size=(350, 350),
+    batch_size=batch_size,
+    class_mode='categorical',
+    color_mode='grayscale'
+)
+class_names = list(train_generator.class_indices.keys())
+print(class_names)
+model = Sequential()
+# convolutional layers
+model.add(Conv2D(128, kernel_size=(3,3), activation='relu', input_shape=(350,350,1)))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.4))
+
+model.add(Conv2D(256, kernel_size=(3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.4))
+
+model.add(Conv2D(512, kernel_size=(3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.4))
+
+model.add(Conv2D(512, kernel_size=(3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.4))
+
+model.add(Flatten())
+# fully connected layers
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.4))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.3))
+# output layer
+model.add(Dense(27, activation='softmax'))
+model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = 'accuracy' )
+# !rm -rf Logs
+logdir = os.path.join("Logs")
+tensorboard_callback = TensorBoard(log_dir=logdir)
+
+model.fit(
+    train_generator,
+    steps_per_epoch=train_generator.samples // batch_size,
+    epochs=100,
+    validation_data=validation_generator,
+    validation_steps=validation_generator.samples // batch_size,
+    callbacks=[tensorboard_callback]
+)
+model_json = model.to_json()
+with open("signlanguagedetectionmodel48x48.json",'w') as json_file:
+    json_file.write(model_json)
+model.save("signlanguagedetectionmodel48x48.h5")
